2a47c5b2c99250491615b9589bb0cbec967ac2b2
RaideNaeNae <raidenlouie@gmail.com>
2025-03-08 14:34:47 -0800

    Update week4_assignment2.cpp

--- week4_assignment2.cpp
+++ week4_assignment2.cpp
@@ -1,3 +1,80 @@
+// #include "main.h"
+
+// DigitalOut led(L27);
+// DigitalOut led2(L26);
+// DigitalOut led3(L25);
+// DigitalOut ledbuiltin(LED1);
+
+// DJIMotor testMot(5, CANHandler::CANBUS_1, GM6020, "testbench_motor");
+
+// #define IMPULSE_DT 100
+// #define IMPULSE_STRENGTH 16383
+
+// int main(){
+
+//     DJIMotor::s_setCANHandlers(&canHandler1, &canHandler2, false, false);
+//     DJIMotor::s_sendValues();
+//     DJIMotor::s_getFeedback();
+
+//     unsigned long timeStart;
+//     unsigned long loopTimer = us_ticker_read();
+//     int refLoop = 0;
+
+//     testMot.setSpeedPID(1.5,0,200);
+
+//     bool prevL = false;
+//     bool switL = false;
+
+//     int motorSpeed = 0;
+
+//     while(true){
+//         timeStart = us_ticker_read();
+
+//         if ((timeStart - loopTimer) / 1000 > 25){
+//             loopTimer = timeStart;
+//             led = !led;
+//             ledbuiltin = !ledbuiltin;
+
+//             refLoop++;
+//             if (refLoop >= 5){
+//                 refereeThread(&referee);
+//                 refLoop = 0;
+//                 led2 = !led2;
+//             }
+//             prevL = switL;
+//             remoteRead();
+//             switL = (remote.leftSwitch() == Remote::SwitchState::UP);
+
+//             // testMot.setPower(remote.leftX() * 3);
+//             // if(!prevL && switL){
+//             //     motorSpeed += 10;
+//             // }
+
+//             motorSpeed = remote.leftX() / 6;
+
+//             testMot.setSpeed(motorSpeed);
+//             int dir = 0;
+//             if(motorSpeed > 0){
+//                 dir = 1;
+//             }else if(motorSpeed < 0){
+//                 dir = -1;
+//             }
+//             testMot.pidSpeed.feedForward = dir * (874 + (73.7 * abs(motorSpeed)) + (0.0948 * motorSpeed * motorSpeed));
+//             printff("%d\t%d\t%d\n", testMot>>POWEROUT, motorSpeed, testMot>>VELOCITY);
+
+//             DJIMotor::s_sendValues();
+//         }
+//         DJIMotor::s_getFeedback();
+//         ThisThread::sleep_for(1ms);
+//     }
+// }
+
+//STARTER 
+//THIS EXERCISE IS TO BE DONE AND THEN RUN IN REAL LIFE ON A ROBOT
+//YOU WILL NOT BE ABLE TO COMPILE THIS UNLESS YOU SET UP THE BUILD ENVIRONMENTrobots/TestBench/main.cpp
+//STARTER 
+//THIS EXERCISE IS TO BE DONE AND THEN RUN IN REAL LIFE ON A ROBOT
+//YOU WILL NOT BE ABLE TO COMPILE THIS UNLESS YOU SET UP THE BUILD ENVIRONMENT
 #include "main.h"
 
 DigitalOut led(L26);
@@ -7,13 +84,13 @@ DigitalOut led3(L25);
 I2C i2c(I2C_SDA, I2C_SCL);
 
 //DEFINE MOTORS, ETC
-int maxRPM = 9000;
-    int maxRemote = 660;
-    double scaleFactor = 9000/660;
-    DJIMotor tl(1, CANHandler::CANBUS_1, M3508, "top left motor");
-    DJIMotor tr(2, CANHandler::CANBUS_1, M3508, "top right motor");
-    DJIMotor bl(3, CANHandler::CANBUS_1, M3508, "bottom left motor");
-    DJIMotor br(4, CANHandler::CANBUS_1, M3508, "bottom right motor");
+float maxRPM = 9000.0;
+float maxRemote = 660.0;
+double scaleFactor = 9000/660;
+DJIMotor tl(1, CANHandler::CANBUS_1, M3508, "top left motor");
+DJIMotor tr(2, CANHandler::CANBUS_1, M3508, "top right motor");
+DJIMotor bl(3, CANHandler::CANBUS_1, M3508, "bottom left motor");
+DJIMotor br(4, CANHandler::CANBUS_1, M3508, "bottom right motor");
 
 int main(){
 
@@ -52,10 +129,6 @@ int main(){
             int x = remote.leftX();
             int y = remote.leftY();
             int r = remote.rightX();
-
-            if(x<50&&x>-50) {x=0;}
-            if(y<50&&y>-50) {y=0;}
-            if(r<50&&r>-50) {r=0;}
 
             //printff("%d %d\n", x, y);
 
